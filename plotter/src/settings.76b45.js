fd2594319e87673d5f89b5895e230b1c3872a132
烧风 <hk-shao@foxmail.com>
2021-06-13 20:53:59 +0800

    Site updated: 2021-06-13 20:53:58

---
+++ plotter/src/settings.76b45.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/main.fire",orientation:"",jsList:[],bundleVers:{internal:"3c089",main:"04c80"}};
