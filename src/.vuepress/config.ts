867d99cbc28865025fc22ba6f63ee182be88cbc6
HK-SHAO <hk-shao@foxmail.com>
2022-10-07 15:57:24 +0800

    update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,22 +1,33 @@
 import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from 'vuepress'
 
 export default defineUserConfig({
-    lang: 'zh-CN',
-    title: 'HK-SHAO',
-    description: '烧风的个人网站',
-    head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
-    locales: {
-        // 键名是该语言所属的子路径
-        // 作为特例，默认语言可以使用 '/' 作为其路径。
-        '/': {
-            lang: 'zh-CN',
-            title: 'HK-SHAO',
-            description: '烧风的个人网站',
-        },
-        '/en/': {
-            lang: 'en-US',
-            title: "HK-SHAO",
-            description: "shaofun's personal website",
-        },
+  lang: 'zh-CN',
+  title: 'HK-SHAO',
+  description: '烧风的个人网站',
+  head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'zh-CN',
+      title: 'HK-SHAO',
+      description: '烧风的个人网站',
     },
+    '/en/': {
+      lang: 'en-US',
+      title: "HK-SHAO",
+      description: "shaofun's personal website",
+    },
+  },
+  theme: defaultTheme({
+    locales: {
+      '/': {
+        selectLanguageName: '简体中文',
+      },
+      '/en/': {
+        selectLanguageName: 'English',
+      },
+    },
+  }),
 })
