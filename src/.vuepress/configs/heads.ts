7ca9a96b40d9794877a75c4b8f98bc17a73c4b6d
HK-SHAO <hk-shao@foxmail.com>
2022-10-09 16:03:53 +0800

    update icons

--- src/.vuepress/configs/heads.ts
+++ src/.vuepress/configs/heads.ts
@@ -5,4 +5,12 @@ export const heads: HeadConfig[] = [
     ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
     ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg' }],
     ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" }],
+    ['link', { rel: "manifest", href: "/site.webmanifest" }],
+    ['link', { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#3eaf7c" }],
+    ['meta', { name: "msapplication-TileColor", content: "#3eaf7c" }],
+    ['meta', { name: "theme-color", content: "#ffffff" }],
 ]
